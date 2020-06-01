82571ef45cbfbb6904956db20f7902b9ad0fd198
Sacha Carle <carle.sacha@gmail.com>
2020-06-01 09:58:55 +0200

    shadow entity

---
+++ nu_entity/common.py
@@ -0,0 +1,10 @@
+def merge(source, destination):
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge(value, node)
+        else:
+            destination[key] = value
+
+    return destination
--- nu_entity/instances_class.py
+++ nu_entity/instances_class.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from inspect import ismodule
 import os
 
+from .common import merge
+
 
 class CallInstance(list):
     def __repr__(self):
@@ -58,20 +60,33 @@ class ScopeInstance(dict):
             # elif key in __module__ ???
             else: return SymboleInstance(key)
 
-        def __fs_scan__(self, d, kls):
+        def __fs_scan__(self, d, kls, FIRST=True):
+            body = {}
+            if FIRST:
+                if 'ENTITY_PATH' in kls.__dict__.keys():
+                    d = Path(kls.ENTITY_PATH).resolve().absolute()
+                if 'SHADOW_PATH' in kls.__dict__.keys():
+                    shadow_body = self.__fs_scan__(Path(kls.SHADOW_PATH).resolve().absolute(), type(None))
+                    body = merge(body, shadow_body)
             assert d.exists(), "Entity " + str(d) + ' does not exists'
             assert d.is_dir(), "Can't scan " + str(d) + ", it's not a folder"
-            body = {
-                '__home__': d
-            }
+            body['__home__'] = d
             for l in os.listdir(d):
                 if l.startswith('__') or l.endswith('__'):
                     continue
                 elif os.path.isdir(Path(d, l)):
                     if l == '.entity':
-                        body['__entity__'] = self.__fs_scan__(Path(d, l), kls)
+                        res = self.__fs_scan__(Path(d, l), kls, FIRST=False)
+                        if '__entity__' in body.keys():
+                            body['__entity__'] = merge(res, body['__entity__'])
+                        else:
+                            body['__entity__'] = res
                     elif not l.startswith('.'):
-                        body['__' + str(l)] = self.__fs_scan__(Path(d, l), kls)
+                        res = self.__fs_scan__(Path(d, l), kls, FIRST=False)
+                        if '__' + str(l) in body.keys():
+                            body['__' + str(l)] = merge(res, body['__' + str(l)])
+                        else:
+                            body['__' + str(l)] = res
                 else:
                     body[l.replace('.', '_')] = Path(d, l).resolve().absolute()
             return body
@@ -133,10 +148,13 @@ class EntityInstance:
         self.__dict__ = self.__class__.EntityScopeInstance(self, **kwargs)
         if '__entity__' in self.__dict__.keys():
             orders = sorted(self.__dict__['__entity__'].keys())
+            name = "\t"
+            if '__home__' in self.__dict__.keys():
+                name = os.path.split(str(self.__home__))[-1]
             for k in orders:
                 if k.startswith('__'): continue
                 if k.endswith('_py'):
-                    print ('\t\t', k)
+                    print (name, '\t\t', k)
                     self.__exec__(self.__entity__[k].read_text())
 
     def __exec__(self, code, **kwargs):
